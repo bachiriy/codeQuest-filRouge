7270c7064412696a903ec0be41ce4db8c7a697f8
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-12-24 11:19:02 +0100

    init structre

---
+++ api/src/test/java/com/codequest/api/CodeQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codequest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
