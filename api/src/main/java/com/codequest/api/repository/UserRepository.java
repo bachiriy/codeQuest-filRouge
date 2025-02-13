71b0a81ae93ee102c249f63728992accde2db665
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2025-02-13 15:26:22 +0100

    add user/role repo

---
+++ api/src/main/java/com/codequest/api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codequest.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codequest.api.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+}
